0b76065cc3f29b39dfbd8513f5b5d4d1da4b389b
YanzeJiang <yjia0258@student.monash.edu>
2025-08-26 19:25:15 +1000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import Form from './components/Form.vue'
+</script>
+
+<template>
+
+
+  <Form /> 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+createApp(App).mount('#app')
